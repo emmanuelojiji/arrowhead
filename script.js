56e13a275d2abfc2d646f2d212b7731e6d6ac3a3
Emmanuel <85620559+emmanuelojiji@users.noreply.github.com>
2021-10-04 11:42:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = () => {
   const header = document.querySelector(".header");
   const menuArrow = document.querySelector(".menu-arrow");
   const mobileNav = document.querySelector(".mobile-nav");
+  const mobileNavContent = document.querySelector(".mobile-nav-content");
 
   menuArrow.onclick = () => {
     if (mobileNav.style.display == "flex") {
@@ -10,12 +11,17 @@ window.onload = () => {
       },320);
       mobileNav.style.height = "0";
       menuArrow.classList.remove('menu-open');
+      mobileNavContent.classList.remove('nav-content-transition');
+      document.documentElement.style.overflow = 'auto';
+     
     } else {
       setTimeout(() => {
         mobileNav.style.height = "80vh";
+        mobileNavContent.classList.add('nav-content-transition');
       });
       mobileNav.style.display = "flex";
       menuArrow.classList.add('menu-open');
+      document.documentElement.style.overflow = 'hidden';
     }
   };
 };
